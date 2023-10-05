2dee3f1ccb212cdc9550ee2f04a5ebb925aa79f5
Furkan Dogan <furkan.dogan@t2med.de>
2023-10-05 12:41:17 +0200

    Pyton datei aktualisiert

--- Hash Aufgabe.py
+++ Hash Aufgabe.py
@@ -1,66 +1,56 @@
 import csv
 import matplotlib.pyplot as plt
-import numpy as np
 
-"""TODO: 1. Anzahl der Tabellengröße bei der Ausführung der Funktion bestimmen können, 
-2. Anzahl der Inputwerte bestimmen
-3. gleichzeitig verändert sich die Länge des Mittelwertes in der midquare methode. 
-4. Histogramm bauen """
-
-
-
-def read_csv_file(file_path):
+def read_csv_file(file_path, num_rows):
     rows = []
     with open(file_path, 'r', encoding='ISO-8859-1') as file:
         csv_reader = csv.reader(file)
-        row_count = 0
-        for row in csv_reader:
-            rows.append(row)
-            row_count += 1
-            if row_count == 50: 
-                break
+        for _ in range(num_rows):
+            rows.append(next(csv_reader))
     return rows
 
 def key_Wert(input_Wert):
     key = 0
     for buchstabe in input_Wert:
         key += ord(buchstabe)
-    return key  # Die Rückgabe sollte außerhalb der Schleife erfolgen
+    return key
 
-def mid_square_hash(input_key: int):
-    hashsize = 1000
+def mid_square_hash(input_key: int, hashsize):
     squared = input_key * input_key
     str_squared = str(squared)
     length = len(str_squared)
-    start = (length - 3) // 2
-    end = start + 3
+    num_digits = len(str(hashsize))
+    start = (length - num_digits) // 2
+    end = start + num_digits
     mid_value = int(str_squared[start:end])
     return mid_value % hashsize
 
 file_path = "/Users/furkan/Downloads/archive/de_DE.csv"
-rows = read_csv_file(file_path)
+
+hashsize = int(input("Bitte geben Sie die gewünschte Hashgröße ein: "))
+num_rows = int(input("Bitte geben Sie die Anzahl der Inputwerte ein: "))
+
+rows = read_csv_file(file_path, num_rows)
 
 hashliste = set()
-dublikat = []
+collision_count = {i: 0 for i in range(hashsize)}
 
 for row in rows:
     key = key_Wert(str(row))
-    hashwert = mid_square_hash(key)
-    while hashwert in hashliste:  # Wenn der Hashwert bereits existiert, behandeln Sie die Kollision
-        dublikat.append(hashwert)
-        hashwert = mid_square_hash(hashwert)  # Eine einfache Methode, um die Kollision zu behandeln
-    hashliste.add(hashwert)
+    hashwert = mid_square_hash(key, hashsize)
+    if hashwert in hashliste:
+        collision_count[hashwert] += 1
+    else:
+        hashliste.add(hashwert)
+        collision_count[hashwert] = 1 
+        
 
-if dublikat:
-    print("Folgende Werte sind Dublikate:")
-    for wert in dublikat:
-        print(wert)
-else: 
-    print("Keine Dublikate gefunden")
+used_slots = {k: v for k, v in collision_count.items() if v >= 1}
 
-gaussian_numbers = np.random.normal(size= 10000)
-plt.hist(gaussian_numbers)
-plt.title("Gaussian Histogram")
-plt.xlabel("Tabelle")
-plt.ylabel("Kollisionen")
+# Histogramm erstellen
+plt.bar(used_slots.keys(), used_slots.values(), color='blue')
+plt.xlabel('Tabellenplätze')
+plt.ylabel('Anzahl der Kollisionen/Einträge')
+plt.title('Histogramm der Kollisionen')
+plt.yticks(range(1, 6))  
 plt.show()
